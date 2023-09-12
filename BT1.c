7bd3b0031205ea416afa3372f6ea07e31f6390c4
Tungaptech <nghiemthanhtung2013@gmail.com>
2023-09-12 23:26:42 +0700

    Hello T

---
+++ BT1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char*argv[]){
+	//insret code here...
+int a;
+printf("Hello, World!\n");
+scanf("%d", &a);
+printf("Hello, World! %d\n", a);
+	return 0;
+}
---
+++ Vidu01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("Hello C Programming");
+	printf("\nNhap a=");
+	scanf("%d",&a);
+	
+	getch();
+}
